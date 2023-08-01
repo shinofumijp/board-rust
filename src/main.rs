8045732b691e7a7d01759b279a79f9f35aa7d807
shinofumijp <electric.spanish.shinofumi.335@gmail.com>
2023-08-01 23:05:16 +0900

    Add comments

--- src/main.rs
+++ src/main.rs
@@ -27,22 +27,26 @@ fn main() {
 
 #[get("/")]
 fn index(cookies: Cookies) -> Result<Template, Redirect> {
+    // Check if the user has logged in.
     let user = check_cookie(cookies);
-    if user.is_none() {
+    if user.is_err() {
         return Err(Redirect::to("/users/new"));
     }
+
+    // Create context.
     let mut context = HashMap::<String, String>::new();
 
+    // Get list of posts.
     use board_rust::schema::posts::dsl::*;
     let mut conn = db::establish_connection().get().unwrap();
-
-    context.insert("user".to_string(), serde_json::to_string(&user).unwrap());
     let post_list = posts
         .order(published_at.desc())
         .limit(10)
         .load::<Post>(&mut conn)
         .expect("Error loading posts");
 
+    // Insert user and post list into context.
+    context.insert("user".to_string(), serde_json::to_string(&user).unwrap());
     context.insert(
         "posts".to_string(),
         serde_json::to_string(&post_list).unwrap(),
@@ -50,27 +54,29 @@ fn index(cookies: Cookies) -> Result<Template, Redirect> {
 
     println!("{:?}", context);
 
+    // Render index page.
     Ok(Template::render("index", &context))
 }
 
-fn check_cookie(mut cookies: Cookies) -> Option<User> {
-    use board_rust::schema::users::dsl::*;
-
+fn check_cookie(mut cookies: Cookies) -> Result<User, String> {
+    // Get user_id from cookie.
     let cookie = cookies.get_private("user_id");
     if cookie.is_none() {
-        return None;
+        return Err("No user_id cookie".to_string());
     }
-
     let user_id = cookie.unwrap().value().parse::<i32>().unwrap();
 
+    // Get user from database.
+    use board_rust::schema::users::dsl::*;
     let mut conn = db::establish_connection().get().unwrap();
-    let user = users.filter(id.eq(user_id)).first::<User>(&mut conn);
+    let user = users.find(user_id).get_result::<User>(&mut conn);
 
+    // Check if the user exists.
     match user {
-        Ok(u) => Some(u),
-        Err(_) => {
+        Ok(u) => Ok(u),
+        Err(e) => {
             cookies.remove_private(Cookie::named("user_id"));
-            None
+            Err(e.to_string())
         }
     }
 }
@@ -86,22 +92,28 @@ fn create_user(
     mut cookies: Cookies,
     user_form: Form<UserForm>,
 ) -> Result<Redirect, Flash<Redirect>> {
-    use board_rust::schema::users;
-
+    // Hash the password.
     let password_hash = bcrypt::hash(&user_form.password, bcrypt::DEFAULT_COST).unwrap();
+
+    // Get the database connection.
     let mut conn = db::establish_connection().get().unwrap();
 
+    // Create the new user.
     let new_user = NewUser {
         name: &user_form.name.to_string(),
         email: &user_form.email.to_string(),
         password: &password_hash,
     };
 
+    use board_rust::schema::users;
+
+    // Insert the new user into the database.
     let user = diesel::insert_into(users::table)
         .values(&new_user)
         .get_result::<User>(&mut conn)
         .map_err(|_| Flash::error(Redirect::to("/users/new"), "Failed to create user."))?;
 
+    // Set a cookie with the user's id.
     cookies.add_private(Cookie::new("user_id", user.id.to_string()));
 
     Ok(Redirect::to("/"))
